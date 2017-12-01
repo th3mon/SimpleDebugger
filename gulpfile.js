5c52f458a9eef5a91a4456b4b0b9ae2988cdf6bc
th3mon <p.szelenberger@gmail.com>
2017-12-01 08:24:25 +0100

    Added eslint and fixed lint issues.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const gulpLoadPlugins = require('gulp-load-plugins');
 const plugins = gulpLoadPlugins();
 const browserSync = require('browser-sync').create();
 
-gulp.task('build', () =>
+gulp.task('build', ['lint'], () =>
   gulp.src('./src/*.js')
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.uglify())
@@ -15,13 +15,19 @@ gulp.task('build', () =>
 gulp.task('browser-sync', () =>
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: './'
     }
   })
 );
 
-gulp.task('serve', () => {
-  gulp.run('browser-sync');
+gulp.task('lint', () => {
+  return gulp.src(['**/*.js', '!node_modules/**', '!dist/**'])
+    .pipe(plugins.eslint())
+    .pipe(plugins.eslint.format())
+    .pipe(plugins.eslint.failAfterError());
+});
+
+gulp.task('serve', ['browser-sync', 'lint'], () => {
   gulp.watch('./src/*', ['build'])
     .on('change', browserSync.reload);
 });
--- src/simple-debugger.js
+++ src/simple-debugger.js
@@ -4,45 +4,44 @@
 **  Dependencies
 **      jQuery v1.8
 */
-(function (g, d, $, u){
-    var SimpleDebugger = function(id) {
-        var
-            self = this,
-            createMainContainer = function() {
-                self.$main = $('<div>', {
-                    id: 'SimpleDebugger' + id,
-                    'class': 'SimpleDebugger'
-                }).appendTo(d.body);
-            };
-
-        this.id = id;
-        this.messageId = 0;
-        this.messages = [];
-        createMainContainer();
-        $(d.body).addClass('SimpleDebuggerOnBoard');
-        g.onerror = function(e, src, line) {
-            self.add('error: ' + e);
-            self.add('in ' + src + ' at line ' + line);
-        };
+(function (g, d, $, u) {
+  const SimpleDebugger = function (id) {
+    const self = this;
+    const createMainContainer = function () {
+      self.$main = $('<div>', {
+        id: 'SimpleDebugger' + id,
+        'class': 'SimpleDebugger'
+      }).appendTo(d.body);
     };
 
-    SimpleDebugger.prototype.add = function(message) {
-        var messageConfig = {
-            id: 'SimpleDebuggerMessage' + this.id + this.messageId,
-            text: message
-        };
+    this.id = id;
+    this.messageId = 0;
+    this.messages = [];
+    createMainContainer();
+    $(d.body).addClass('SimpleDebuggerOnBoard');
+    g.onerror = function (e, src, line) {
+      self.add('error: ' + e);
+      self.add('in ' + src + ' at line ' + line);
+    };
+  };
 
-        $('<p>', messageConfig).appendTo(this.$main);
-        this.messages.push(messageConfig);
-        this.messageId += 1;
-
-        $(d.body).css({paddingTop: this.$main.height() + 'px'});
+  SimpleDebugger.prototype.add = function (message) {
+    const messageConfig = {
+      id: 'SimpleDebuggerMessage' + this.id + this.messageId,
+      text: message
     };
 
-    SimpleDebugger.prototype.remove = function(messageId) {
-        $('#' + messageId).remove();
-        $(d.body).css({paddingTop: this.$main.height() + 'px'});
-    };
+    $('<p>', messageConfig).appendTo(this.$main);
+    this.messages.push(messageConfig);
+    this.messageId += 1;
 
-    g.SimpleDebugger = SimpleDebugger;
+    $(d.body).css({paddingTop: this.$main.height() + 'px'});
+  };
+
+  SimpleDebugger.prototype.remove = function (messageId) {
+    $('#' + messageId).remove();
+    $(d.body).css({paddingTop: this.$main.height() + 'px'});
+  };
+
+  g.SimpleDebugger = SimpleDebugger;
 }(window, document, jQuery));
