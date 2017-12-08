3283e97490e066511d6db6b25f6c3102a282fd37
th3mon <p.szelenberger@gmail.com>
2017-12-08 04:57:08 +0100

    Added jQuery and made some fixes.

--- src/index.js
+++ src/index.js
@@ -1,43 +1,42 @@
 import '../css/main.css';
+import $ from 'jquery';
 
-(function (g, d, $, u) {
-  const SimpleDebugger = function (id) {
-    const self = this;
-    const createMainContainer = function () {
-      self.$main = $('<div>', {
-        id: 'SimpleDebugger' + id,
-        'class': 'SimpleDebugger'
-      }).appendTo(d.body);
-    };
-
-    this.id = id;
-    this.messageId = 0;
-    this.messages = [];
-    createMainContainer();
-    $(d.body).addClass('SimpleDebuggerOnBoard');
-    g.onerror = function (e, src, line) {
-      self.add('error: ' + e);
-      self.add('in ' + src + ' at line ' + line);
-    };
+const SimpleDebugger = function (id) {
+  const self = this;
+  const createMainContainer = function () {
+    self.$main = $('<div>', {
+      id: 'SimpleDebugger' + id,
+      'class': 'SimpleDebugger'
+    }).appendTo(document.body);
   };
 
-  SimpleDebugger.prototype.add = function (message) {
-    const messageConfig = {
-      id: 'SimpleDebuggerMessage' + this.id + this.messageId,
-      text: message
-    };
+  this.id = id;
+  this.messageId = 0;
+  this.messages = [];
+  createMainContainer();
+  $(document.body).addClass('SimpleDebuggerOnBoard');
+  window.onerror = function (e, src, line) {
+    self.add('error: ' + e);
+    self.add('in ' + src + ' at line ' + line);
+  };
+};
 
-    $('<p>', messageConfig).appendTo(this.$main);
-    this.messages.push(messageConfig);
-    this.messageId += 1;
-
-    $(d.body).css({paddingTop: this.$main.height() + 'px'});
+SimpleDebugger.prototype.add = function (message) {
+  const messageConfig = {
+    id: 'SimpleDebuggerMessage' + this.id + this.messageId,
+    text: message
   };
 
-  SimpleDebugger.prototype.remove = function (messageId) {
-    $('#' + messageId).remove();
-    $(d.body).css({paddingTop: this.$main.height() + 'px'});
-  };
+  $('<p>', messageConfig).appendTo(this.$main);
+  this.messages.push(messageConfig);
+  this.messageId += 1;
 
-  g.SimpleDebugger = SimpleDebugger;
-}(window, document, jQuery));
+  $(document.body).css({paddingTop: this.$main.height() + 'px'});
+};
+
+SimpleDebugger.prototype.remove = function (messageId) {
+  $('#' + messageId).remove();
+  $(document.body).css({paddingTop: this.$main.height() + 'px'});
+};
+
+window.SimpleDebugger = SimpleDebugger;
