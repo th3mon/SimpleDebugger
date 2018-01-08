2d8ac29f8216259bc4e0a34f95b1b7a56c5b583f
th3mon <p.szelenberger@gmail.com>
2018-01-08 11:11:29 +0100

    Use ES6, move functions into class instead of addMainClass().

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const createMainContainer = id => {
 
   return mainContainer;
 };
+const addMainClass = () => document.body.classList.add('SimpleDebuggerOnBoard');
 
 class SimpleDebugger {
   constructor (window, id) {
@@ -20,44 +21,40 @@ class SimpleDebugger {
     this.mainContainer = createMainContainer(id);
     // TODO: should throw error if document is falsy
     document.body.appendChild(this.mainContainer);
-    this.addMainClass();
+    addMainClass();
     // TODO: should throw error if window is falsy
     window.onerror = (e, src, line) => this.logError(e, src, line);
   }
+
+  add (message) {
+    const messageConfig = {
+      id: `SimpleDebuggerMessage-${this.id}-${this.messageId}`,
+      text: message
+    };
+
+    this.messages.push(messageConfig);
+    const messageElement = this.createMessageElement(messageConfig);
+
+    this.addMessageToDOM(messageElement);
+    this.updateHeightOfContainer();
+
+    this.messageId += 1;
+  }
+
+  createMessageElement ({ id, text }) {
+    const pararaph = document.createElement('p');
+
+    pararaph.setAttribute('id', id);
+    pararaph.innerText = text;
+    pararaph.classList.add(id, 'SimpleDebugger__message');
+
+    return pararaph;
+  }
+
+  addMessageToDOM (messageElement) {
+    this.mainContainer.appendChild(messageElement);
+  }
 }
-
-SimpleDebugger.prototype.addMainClass = function () {
-  document.body.classList.add('SimpleDebuggerOnBoard');
-};
-
-SimpleDebugger.prototype.add = function (message) {
-  const messageConfig = {
-    id: `SimpleDebuggerMessage-${this.id}-${this.messageId}`,
-    text: message
-  };
-
-  this.messages.push(messageConfig);
-  const messageElement = this.createMessageElement(messageConfig);
-
-  this.addMessageToDOM(messageElement);
-  this.updateHeightOfContainer();
-
-  this.messageId += 1;
-};
-
-SimpleDebugger.prototype.addMessageToDOM = function (messageElement) {
-  this.mainContainer.appendChild(messageElement);
-};
-
-SimpleDebugger.prototype.createMessageElement = function ({ id, text }) {
-  const pararaph = document.createElement('p');
-
-  pararaph.setAttribute('id', id);
-  pararaph.innerText = text;
-  pararaph.classList.add(id, 'SimpleDebugger__message');
-
-  return pararaph;
-};
 
 SimpleDebugger.prototype.updateHeightOfContainer = function () {
   const mainContainerHeight = Math.max(this.mainContainer.offsetHeight, this.mainContainer.clientHeight);
